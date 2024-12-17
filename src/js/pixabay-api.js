ad24d1ff8accc5448755a51faf288ae1a33b3a24
Lylyneru <lyly777@gmail.com>
2024-12-17 14:52:36 +0200

    add key

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -25,7 +25,7 @@
 // }
 
 export function fetchImages(query) {
-  const API_KEY = 'YOUR_API_KEY';
+  const API_KEY = '47392920-efce9c2b3427e3353db7767ab';
   const BASE_URL = 'https://pixabay.com/api/';
   const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
 
